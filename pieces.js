e322af9545fd62e9309d69a7a6da67bb8e5fc481
Ariel Yamien <arielyamien770@gmail.com>
2025-06-03 09:01:34 +0000

    crétion d'une boucle for pour récuperer tout les éléments automatiquement

--- pieces.js
+++ pieces.js
@@ -1,38 +1,37 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
+//Récuperer les éléments du json
 const pieces = await reponse.json();
 
-//Récuperer le premier élément du json
-const article = pieces[0];
+for(let i = 0 ; i < pieces.length; i++) {
+    //Récuperation de la balise parente 'section'
+    const sectionFiches = document.querySelector(".fiches");
+    //création d'une balise article
+    const pieceElement = document.createElement("article");
 
-//Création des balises
-const imgElement = document.createElement("img");
-imgElement.src = article.image;
+    //Création des balises
+    const imgElement = document.createElement("img");
+    const nomElement = document.createElement("h2");
+    const prixElement = document.createElement("p");
+    const categorieElement = document.createElement("p");
+    const descriptionElement = document.createElement("p");
+    const dispoibiliteElement = document.createElement("p");
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-
-const prixElement = document.createElement("p");
-//Opérateur ternaire
-prixElement.innerText = `Prix : ${article.prix} € ${(article.prix < 35 ? "€" : "(€€€)")} `;
-
-const categorieElement = document.createElement("p");
-//opération nullish
-categorieElement.innerText = article.categorie ?? "(Aucune categorie)";
-
-const descriptionElement = document.createElement("p");
-//opération nullish
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
-
-const dispoibiliteElement = document.createElement("p");
-//opérateur ternaire
-dispoibiliteElement.innerText = `${article.disponibilite == true ? "En stock" : "Rupture de stock"}`;
-
-//Ajout des balises crée dans le parent section
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imgElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(dispoibiliteElement);
+    // On accède à l’indice i de la liste pieces pour configurer les sources.
+    imgElement.src = pieces[i].image;
+    nomElement.innerText = pieces[i].nom;
+    //Opérateur ternaire
+    prixElement.innerText = `Prix : ${pieces[i].prix} € ${(pieces[i].prix < 35 ? "(€)" : "(€€€)")} `;
+    //opération nullish
+    categorieElement.innerText = pieces[i].categorie ?? "(Aucune categorie)";
+    //opération nullish
+    descriptionElement.innerText = pieces[i].description ?? "(Pas de description pour le moment)";
+    //opérateur ternaire
+    dispoibiliteElement.innerText = `${pieces[i].disponibilite == true ? "En stock" : "Rupture de stock"}`;
+    sectionFiches.appendChild(pieceElement);
+    pieceElement.appendChild(imgElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(dispoibiliteElement);
+}
