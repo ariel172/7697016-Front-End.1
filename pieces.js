86bd1807d16d34ffb970fbb2ada5cf227ab747a4
Ariel Yamien <arielyamien770@gmail.com>
2025-06-03 10:56:17 +0000

    Récupertion des bouton et ajout dans le HTML pour trier par ordre décroissant et par description

--- pieces.js
+++ pieces.js
@@ -27,7 +27,7 @@ for(let i = 0 ; i < pieces.length; i++) {
     //opération nullish
     descriptionElement.innerText = pieces[i].description ?? "(Pas de description pour le moment)";
     //opérateur ternaire
-    dispoibiliteElement.innerText = `${pieces[i].disponibilite == true ? "En stock" : "Rupture de stock"}`;
+    dispoibiliteElement.innerText = `${pieces[i].disponibilite === true ? "En stock" : "Rupture de stock"}`;
     sectionFiches.appendChild(pieceElement);
     pieceElement.appendChild(imgElement);
     pieceElement.appendChild(nomElement);
@@ -35,3 +35,43 @@ for(let i = 0 ; i < pieces.length; i++) {
     pieceElement.appendChild(categorieElement);
     pieceElement.appendChild(dispoibiliteElement);
 }
+
+const boutonTrier = document.querySelector(".btn-trier");
+//Gestion de l'évènement click sur le bouton trier
+boutonTrier.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces)
+      piecesOrdonnees.sort(function(a,b){
+        //Tri des pièces par prix croissant
+        return a.prix - b.prix;
+    }),
+    console.log(piecesOrdonnees)
+})
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");  
+//Gestion de l'évènement click sur le bouton filtrer
+boutonFiltrer.addEventListener("click", () =>{
+    const piecesFiltrees = pieces.filter(function(pieces){
+        //Filtre les pièces dont le prix est inférieur à 35€
+        return pieces.prix <= 35;
+    });
+    console.log(piecesFiltrees);
+})
+
+const boutonFiltrerDesciption = document.querySelector(".btn-descripton");
+//Gestion de l'évènement click sur le bouton filtrer description
+boutonFiltrerDesciption.addEventListener("click", () =>{
+    const descriptionFiltrees = pieces.filter(function(pieces){
+        return pieces.description
+    })
+    console.log(descriptionFiltrees);
+})
+
+const boutonOrdreDecroissant = document.querySelector(".btn-decroissant")
+//Gestion de l'évènement click sur le bouton ordre décroissant
+boutonOrdreDecroissant.addEventListener("click", () =>{
+    const piecesDecroissantes = Array.from(pieces)
+    piecesDecroissantes.sort(function(a,b){
+        return b.prix - a.prix
+    })
+    console.log(piecesDecroissantes)
+})
